9bf6789c82f677d12bb9f2faab966cd7b51125f7
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-04-08 19:46:19 +0200

    refactor: monitoring routines for better readability and structure

--- src/monitoring.c
+++ src/monitoring.c
@@ -6,11 +6,27 @@
 /*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/30 19:18:40 by tsargsya          #+#    #+#             */
-/*   Updated: 2025/04/08 14:55:01 by tsargsya         ###   ########.fr       */
+/*   Updated: 2025/04/08 19:39:37 by tsargsya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+static void	set_simulation_end(t_vars *vars)
+{
+	pthread_mutex_lock(&vars->simulation_lock);
+	vars->simulation_end = 1;
+	pthread_mutex_unlock(&vars->simulation_lock);
+}
+
+static void	handle_all_philos_satisfied(t_vars *vars)
+{
+	pthread_mutex_lock(&vars->print_lock);
+	printf("Each philosopher ate %d time(s)\n", vars->rules.must_eat_count);
+	pthread_mutex_unlock(&vars->print_lock);
+	set_simulation_end(vars);
+	cleanup_and_exit(vars);
+}
 
 int	is_simulation_ended(t_vars *vars)
 {
@@ -22,53 +38,65 @@ int	is_simulation_ended(t_vars *vars)
 	return (end);
 }
 
-void	set_simulation_end(t_vars *vars)
+static int	check_if_any_philo_died(t_vars *vars)
 {
-	pthread_mutex_lock(&vars->simulation_lock);
-	vars->simulation_end = 1;
-	pthread_mutex_unlock(&vars->simulation_lock);
+	int		i;
+	t_philo	*philo;
+	long	time_since_last_meal;
+
+	i = 0;
+	while (i < vars->rules.philo_count)
+	{
+		philo = &vars->philos[i];
+		time_since_last_meal = get_time_in_ms() - philo->last_meal;
+		if (time_since_last_meal > vars->rules.time_to_die)
+		{
+			set_simulation_end(vars);
+			log_action(philo, "died");
+			cleanup_and_exit(vars);
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+static int	check_if_all_philos_ate_enough(t_vars *vars)
+{
+	int		i;
+	int		count;
+	t_philo	*philo;
+
+	if (vars->rules.must_eat_count <= 0)
+		return (0);
+	i = 0;
+	count = 0;
+	while (i < vars->rules.philo_count)
+	{
+		philo = &vars->philos[i];
+		if (philo->meals_eaten >= vars->rules.must_eat_count)
+			count++;
+		i++;
+	}
+	if (count == vars->rules.philo_count)
+	{
+		handle_all_philos_satisfied(vars);
+		return (1);
+	}
+	return (0);
 }
 
 void	*monitor_routine(void *arg)
 {
 	t_vars	*vars;
-	t_philo	*philo;
-	long	time_since_last_meal;
-	int		i;
-	int		full_count;
 
 	vars = (t_vars *)arg;
 	while (!is_simulation_ended(vars))
 	{
-		i = 0;
-		full_count = 0;
-		while (i < vars->rules.philo_count)
-		{
-			philo = &vars->philos[i];
-			time_since_last_meal = get_time_in_ms() - philo->last_meal;
-			if (time_since_last_meal > vars->rules.time_to_die)
-			{
-				set_simulation_end(vars);
-				log_action(philo, "died");
-				cleanup_and_exit(vars);
-				return (NULL);
-			}
-			if (vars->rules.must_eat_count > 0
-				&& philo->meals_eaten >= vars->rules.must_eat_count)
-				full_count++;
-			i++;
-		}
-		if (vars->rules.must_eat_count > 0
-			&& full_count == vars->rules.philo_count)
-		{
-			pthread_mutex_lock(&vars->print_lock);
-			printf("Each philosopher ate %d time(s)\n",
-				vars->rules.must_eat_count);
-			pthread_mutex_unlock(&vars->print_lock);
-			set_simulation_end(vars);
-			cleanup_and_exit(vars);
+		if (check_if_any_philo_died(vars))
 			return (NULL);
-		}
+		if (check_if_all_philos_ate_enough(vars))
+			return (NULL);
 		usleep(500);
 	}
 	cleanup_and_exit(vars);
