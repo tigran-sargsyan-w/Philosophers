794509ee6f8b8e7c83833ac34396a55d66180940
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-04-27 00:11:18 +0200

    refactor: update header comment in main.c for consistency and clarity

--- src/main.c
+++ src/main.c
@@ -6,12 +6,17 @@
 /*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 12:31:48 by tsargsya          #+#    #+#             */
-/*   Updated: 2025/04/26 22:10:29 by tsargsya         ###   ########.fr       */
+/*   Updated: 2025/04/27 00:11:04 by tsargsya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+/**
+ * @brief Entrypoint of the program.
+ * @param argc The number of command line arguments.
+ * @param argv The command line arguments.
+ */
 int	main(int argc, char **argv)
 {
 	t_vars	vars;
