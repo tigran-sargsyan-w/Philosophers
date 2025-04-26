c740c9473eab2b0af1f42c3ea8dced4bfa870bab
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-04-27 00:22:16 +0200

    refactor: enhance function documentation in utils.c for clarity

--- src/utils.c
+++ src/utils.c
@@ -6,12 +6,18 @@
 /*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 22:04:25 by tsargsya          #+#    #+#             */
-/*   Updated: 2025/04/26 22:07:32 by tsargsya         ###   ########.fr       */
+/*   Updated: 2025/04/27 00:22:03 by tsargsya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+/**
+ * @brief Tries to parse an integer from a string.
+ * @param str The string to parse.
+ * @param out Pointer to store the parsed integer.
+ * @return SUCCESS if parsing was successful, ERROR otherwise.
+ */
 int	try_parse_int(const char *str, int *out)
 {
 	long	num;
@@ -34,6 +40,10 @@ int	try_parse_int(const char *str, int *out)
 	return (SUCCESS);
 }
 
+/**
+ * @brief Gets the current time in milliseconds.
+ * @return The current time in milliseconds.
+ */
 long	get_time_in_ms(void)
 {
 	struct timeval	tv;
@@ -42,6 +52,12 @@ long	get_time_in_ms(void)
 	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
 }
 
+/**
+ * @brief Sleeps for a specified duration,
+ * checking if the simulation has ended.
+ * @param duration The duration to sleep in milliseconds.
+ * @param vars The variables structure.
+ */
 void	smart_sleep(long duration, t_vars *vars)
 {
 	long	start;
@@ -55,6 +71,11 @@ void	smart_sleep(long duration, t_vars *vars)
 	}
 }
 
+/**
+ * @brief Logs an action with a timestamp.
+ * @param philo The philosopher structure.
+ * @param msg The message to log.
+ */
 void	log_action(t_philo *philo, const char *msg)
 {
 	t_vars	*vars;
